3a38632c0c109d81f269001d92d90ee42bd12c9e
dmanslick <dvvshah@gmail.com>
2024-07-14 17:44:25 -0400

    Made back link for the post page go to the community tab

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@chakra-ui/react'
 import { useQuery } from '@tanstack/react-query'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { getPost } from '../utils/getPost'
 import { Link as ChakraLink } from '@chakra-ui/react'
 import { ChevronLeft } from 'lucide-react'
@@ -22,7 +22,7 @@ export default function PostPage() {
     return (
         <Box pt='56px'>
             <Box ml={2} mt={3} mb={2}>
-                <ChakraLink href='#/app' display='flex' color='blue.500' alignItems='center'>
+                <ChakraLink as={Link} to='/app/community' display='flex' color='blue.500' alignItems='center'>
                     <ChevronLeft height={24} width={24} />Back
                 </ChakraLink>
             </Box>
